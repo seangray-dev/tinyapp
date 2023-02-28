fcdb9b98faa73c67810c9070f3266beeb59da384
Sean Gray <seangray.dev@gmail.com>
2023-02-28 15:44:19 +0000

    add GET/POST route

--- express_server.js
+++ express_server.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = 8080; // default port 8080
 
+function generateRandomString() {
+  const characters =
+    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let result = '';
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
@@ -25,12 +35,23 @@ app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
 app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: req.params.longURL };
   res.render('urls_show', templateVars);
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send('Ok'); // Respond with 'Ok' (we will replace this)
 });

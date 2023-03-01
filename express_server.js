36b2d85d4453dac9c478993235201b4d41bc3436
Sean Gray <seangray.dev@gmail.com>
2023-03-01 12:54:38 +0000

    login/logout

--- express_server.js
+++ express_server.js
@@ -35,6 +35,12 @@ app.post('/login', (req, res) => {
   res.redirect('/urls');
 });
 
+// route handler for logout
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+});
+
 // route handler to serve URL database as JSON
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
@@ -42,7 +48,7 @@ app.get('/urls.json', (req, res) => {
 
 // route handler to display short URLS
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies.username };
   res.render('urls_index', templateVars);
 });
 
@@ -55,7 +61,7 @@ app.get('/urls/new', (req, res) => {
 app.get('/urls/:id', (req, res) => {
   const id = req.params.id;
   const longURL = urlDatabase[id];
-  const templateVars = { id, longURL };
+  const templateVars = { id, longURL, username: req.cookies.username };
   res.render('urls_show', templateVars);
 });
 

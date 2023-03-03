78f1a3c3d910b5425f63f2412565d3da15f1aca4
Sean Gray <seangray.dev@gmail.com>
2023-03-03 17:11:19 +0000

    refactor + add tests for helper functions

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,6 +1,6 @@
 const { assert } = require('chai');
+const { getUserByEmail, generateRandomString, urlsForUser } = require('../helpers.js');
 
-const { getUserByEmail } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
@@ -25,5 +25,62 @@ describe('getUserByEmail', function() {
   it('should return undefined with a non-existent email', function() {
     const user = getUserByEmail("user3@example.com", testUsers)
     assert.isUndefined(user);
+  });
+});
+
+describe('generateRandomString', function() {
+  it('should return a string with a length of 6', function() {
+    const randomString = generateRandomString();
+    assert.strictEqual(randomString.length, 6);
+  });
+
+  it('should return a different string each time it is called', function() {
+    const randomString1 = generateRandomString();
+    const randomString2 = generateRandomString();
+    assert.notStrictEqual(randomString1, randomString2);
+  });
+});
+
+const testUrlDatabase = {
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "userRandomID"
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "user2RandomID"
+  },
+  "abc123": {
+    longURL: "http://www.example.com",
+    userID: "userRandomID"
+  }
+};
+
+describe('urlsForUser', function() {
+  it('should return an empty object for a user with no URLs', function() {
+    const userUrls = urlsForUser("user3RandomID", testUrlDatabase);
+    const expectedOutput = {};
+    assert.deepEqual(userUrls, expectedOutput);
+  });
+
+  it('should return an object of URLs for a user with URLs', function() {
+    const userUrls = urlsForUser("userRandomID", testUrlDatabase);
+    const expectedOutput = {
+      "b2xVn2": {
+        longURL: "http://www.lighthouselabs.ca",
+        userID: "userRandomID"
+      },
+      "abc123": {
+        longURL: "http://www.example.com",
+        userID: "userRandomID"
+      }
+    };
+    assert.deepEqual(userUrls, expectedOutput);
+  });
+
+  it('should return an empty object for a non-existent user', function() {
+    const userUrls = urlsForUser("user4RandomID", testUrlDatabase);
+    const expectedOutput = {};
+    assert.deepEqual(userUrls, expectedOutput);
   });
 });
